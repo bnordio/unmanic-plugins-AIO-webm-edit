a83b82c9a1bf29cef2f5e88fbfdb8f740a7d3145
Josh.5 <jsunnex@gmail.com>
2021-08-22 14:30:34 +1200

    Add script for adding plugins from other repos. Submodules are messy for maintaining both the official repo and my own. Using git read-tree to manage plugin repos is also messy as the branches are not always clean. This script just manages it for me in a temp directory.

---
+++ pull_in_my_plugins.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+repo_root_path=$(readlink -e $(dirname "${BASH_SOURCE[0]}")/)
+
+plugin_ids=(
+    "encoder_video_h264_libx264"
+    "encoder_video_hevc_libx265"
+)
+
+pushd "${repo_root_path}" &> /dev/null
+
+
+for plugin_id in "${plugin_ids[@]}"; do
+
+    echo "Processing plugin ${plugin_id}"
+    tmp_dir=$(mktemp -d --suffix='-unmanic-plugin')
+
+    # Clone plugin to temp directory
+    echo -e "\n*** Cloning plugin git repo to '${tmp_dir}/${plugin_id}'"
+    git clone --depth=1 --branch master --single-branch "git@github.com:Josh5/unmanic.plugin.${plugin_id}" "${tmp_dir}/${plugin_id}"
+    pushd "${tmp_dir}/${plugin_id}" &> /dev/null
+
+    # Pull files from plugin to this source directory
+    echo -e "\n*** Pulling plugin submodules"
+    git submodule update --init --recursive 
+
+    # Install/update plugin files
+    echo -e "\n*** Installing files from plugin git repo to this repository's source directory"
+    rsync -avh --delete \
+        --exclude='.git/' \
+        --exclude='.gitmodules' \
+        --exclude='.idea/' \
+        "${tmp_dir}/${plugin_id}/" "${repo_root_path}/source/${plugin_id}"
+
+    popd &> /dev/null
+
+done
+
+popd &> /dev/null
